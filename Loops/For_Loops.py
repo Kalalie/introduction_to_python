976c274695cbfd27fa297f0aa12579224258feec
Kalalie <67173676+Kalalie@users.noreply.github.com>
2020-07-01 19:35:56 +0800

    Clean_Up_File_Names

---
+++ Loops/For_Loops.py
@@ -0,0 +1,16 @@
+tea_collection = [
+    "Earl Grey",
+    "Melbourne Breakfast",
+    "Chai",
+    "Peppermint",
+    "Lemon and Ginger",
+    "Strawberry Cream",
+    "Chamomile",
+    "Green",
+    "Dandelion"
+]
+for tea in tea_collection:
+    print(tea)
+    print(f"I have {tea} flavoured tea.")
+
+print("ended loop")
---
+++ Python_Exercise/Lists_Exercise.py
@@ -0,0 +1,13 @@
+foods = [
+    "orange","apple","banana","strawberry","grape","blueberry","passionfruit","mango","kiwifruit",
+    ["carrot","cauliflower","pumpkin"]
+]
+print(foods)
+
+print(foods[0])
+print(foods[2])
+print(foods[-1])
+print(foods[0:3])
+print(foods[-3:-1])
+print()
+print(foods[0][-3])
